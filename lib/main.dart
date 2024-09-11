9babe0463646df53cb580d04b532ffd6e870364f
Musoxon <msx.tuit@gmail.com>
2024-09-11 23:32:30 +0500

    UI changes

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,6 @@ void main() {
   runApp(const MyHomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
-  }
-}
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
